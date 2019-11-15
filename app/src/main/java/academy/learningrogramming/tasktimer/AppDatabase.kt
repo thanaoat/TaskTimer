0c5ac5d143105732d1b2358877e91ae9c6777151
Thananon Kumkrong <thananon.oat@gmail.com>
2019-11-15 12:04:59 +0700

    Initial commit

---
+++ app/src/main/java/academy/learningrogramming/tasktimer/AppDatabase.kt
@@ -0,0 +1,51 @@
+package academy.learningrogramming.tasktimer
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+
+/**
+ * Basic database class for the application.
+ * The only class that should use this is [AppProvider].
+ */
+
+private const val TAG = "AppDatabase"
+
+private const val DATABASE_NAME = "TaskTimer.db"
+private const val DATABASE_VERSION = 3
+
+internal class AppDatabase private constructor(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    init {
+        Log.d(TAG, "AppDatabase: initialising.")
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        // CREATE TABLE Tasks (_id INTEGER PRIMARY KEY NOT NULL, Name TEXT NOT NULL, Description TEXT, SortOrder INTEGER);
+        Log.d(TAG, "onCreate: starts")
+        val sSQL = """CREATE TABLE ${TaskContract.TABLE_NAME} (
+            ${TaskContract.Columns.ID} INTEGER PRIMARY KEY NOT NULL,
+            ${TaskContract.Columns.TASK_NAME} TEXT NOT NULL,
+            ${TaskContract.Columns.TASK_DESCRIPTION} TEXT,
+            ${TaskContract.Columns.TASK_SORT_ORDER} INTEGER);""".replaceIndent(" ")
+        Log.d(TAG, sSQL)
+        db.execSQL(sSQL)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    companion object : SingletonHolder<AppDatabase, Context>(::AppDatabase)
+//    companion object {
+//
+//        @Volatile
+//        private var instance: AppDatabase? = null
+//
+//        fun getInstance(context: Context): AppDatabase =
+//            instance ?: synchronized(this) {
+//                instance ?: AppDatabase(context).also { instance = it}
+//            }
+//    }
+}
