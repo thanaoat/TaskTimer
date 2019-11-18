d1520f6108436c33479a1a45aefeb498057c48c3
Thananon Kumkrong <thananon.oat@gmail.com>
2019-11-18 18:34:16 +0700

    Test insert, update and delete.

--- app/src/main/java/academy/learningrogramming/tasktimer/MainActivity.kt
+++ app/src/main/java/academy/learningrogramming/tasktimer/MainActivity.kt
@@ -1,8 +1,8 @@
 package academy.learningrogramming.tasktimer
 
+import android.content.ContentValues
 import android.os.Bundle
 import android.util.Log
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
@@ -18,11 +18,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+//        testInsert()
+
+//        testUpdate()
+
+//        testUpdateTwo()
+
+//        testDelete()
+
+        testDeleteTwo()
+
         val projection = arrayOf(TasksContract.Columns.TASK_NAME, TasksContract.Columns.TASK_SORT_ORDER)
         val sortColumn = TasksContract.Columns.TASK_SORT_ORDER
 
-        val cursor = contentResolver.query(TasksContract.buildUriFromId(2),
-            projection,
+//        val cursor = contentResolver.query(TasksContract.buildUriFromId(2),
+        val cursor = contentResolver.query(TasksContract.CONTENT_URI,
+            null,
             null,
             null,
             sortColumn)
@@ -31,17 +42,73 @@ class MainActivity : AppCompatActivity() {
             while (it.moveToNext()) {
                 // Cycle through all records
                 with(cursor) {
-//                    val id = getLong(0)
-                    val name = getString(0)
-//                    val description = getString(2)
-                    val sortOrder = getString(1)
-                    val result = "Name: $name sort order: $sortOrder"
+                    val id = getLong(0)
+                    val name = getString(1)
+                    val description = getString(2)
+                    val sortOrder = getString(3)
+                    val result = "ID: $id. Name: $name. description: $description. sort order: $sortOrder"
                     Log.d(TAG,"onCreate: reading data $result")
                 }
             }
         }
         Log.d(TAG, "*****************************")
 
+    }
+
+    private fun testDeleteTwo() {
+
+        val selection = TasksContract.Columns.TASK_DESCRIPTION + " = ?"
+        val selectionArgs = arrayOf("For deletion")
+        val rowsAffected = contentResolver.delete(TasksContract.CONTENT_URI,
+            selection,
+            selectionArgs)
+
+        Log.d(TAG, "Number of rows deleted is $rowsAffected")
+    }
+
+    private fun testDelete() {
+
+        val taskUri = TasksContract.buildUriFromId(3)
+        val rowsAffected = contentResolver.delete(taskUri, null, null)
+        Log.d(TAG, "Number of rows deleted is $rowsAffected")
+    }
+
+    private fun testUpdateTwo() {
+        val values = ContentValues().apply {
+            put(TasksContract.Columns.TASK_SORT_ORDER, 999)
+            put(TasksContract.Columns.TASK_DESCRIPTION, "For deletion")
+        }
+
+        val selection = TasksContract.Columns.TASK_SORT_ORDER + " = ?"
+        val selectionArgs = arrayOf("99")
+        val rowsAffected = contentResolver.update(TasksContract.CONTENT_URI,
+            values,
+            selection,
+            selectionArgs)
+        Log.d(TAG, "Number of rows updated is $rowsAffected")
+    }
+
+    private fun testUpdate() {
+        val values = ContentValues().apply {
+            put(TasksContract.Columns.TASK_NAME, "Content Provider")
+            put(TasksContract.Columns.TASK_DESCRIPTION, "Record content provider videos")
+        }
+
+        val taskUri = TasksContract.buildUriFromId(4)
+        val rowsAffected = contentResolver.update(taskUri, values, null, null)
+        Log.d(TAG, "Number of rows updated is $rowsAffected")
+    }
+
+    private fun testInsert() {
+        val values = ContentValues().apply {
+            put(TasksContract.Columns.TASK_NAME, "New Task 1")
+            put(TasksContract.Columns.TASK_DESCRIPTION, "Description 1")
+            put(TasksContract.Columns.TASK_SORT_ORDER, 2)
+        }
+
+        val uri = contentResolver.insert(TasksContract.CONTENT_URI, values)
+        Log.d(TAG, "New row id (in uri) is $uri")
+        Log.d(TAG, "id (in uri) is ${TasksContract.getId(uri!!)}")
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
