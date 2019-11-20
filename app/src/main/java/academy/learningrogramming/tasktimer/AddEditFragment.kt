a934c4a4f6093f458f2a59e60c4cad329ec13989
Thananon Kumkrong <thananon.oat@gmail.com>
2019-11-20 18:22:46 +0700

    Refactor saveTask()

--- app/src/main/java/academy/learningrogramming/tasktimer/AddEditFragment.kt
+++ app/src/main/java/academy/learningrogramming/tasktimer/AddEditFragment.kt
@@ -10,6 +10,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.fragment_add_edit.*
 
 private  const val TAG = "AddEditFragment"
@@ -28,6 +29,7 @@ private const val ARG_TASK = "task"
 class AddEditFragment : Fragment() {
     private var task: Task? = null
     private var listener: OnSaveClicked? = null
+    private val viewModel by lazy { ViewModelProviders.of(activity!!).get(TaskTimerViewModel::class.java)}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG, "onCreate: starts")
@@ -63,46 +65,26 @@ class AddEditFragment : Fragment() {
         }
     }
 
-    private fun saveTask() {
-        // Update the database if at least one field has changed
-        // - There's no need to hit the database unless this has happened
+    private fun taskFromUi(): Task {
         val sortOrder = if (addedit_sortorder.text.isNotEmpty()) {
             Integer.parseInt(addedit_sortorder.text.toString())
         } else {
             0
         }
 
-        val values = ContentValues()
-        val task = task
+        val newTask = Task(addedit_name.text.toString(), addedit_description.text.toString(), sortOrder)
+        newTask.id = task?.id ?: 0
 
-        if (task != null) {
-            Log.d(TAG, "saveTask: updaing existing task")
-            if (addedit_name.text.toString() != task.name) {
-                values.put(TasksContract.Columns.TASK_NAME, addedit_name.text.toString())
-            }
-            if (addedit_description.text.toString() != task.description) {
-                values.put(TasksContract.Columns.TASK_DESCRIPTION,
-                    addedit_description.text.toString())
-            }
-            if (sortOrder != task.sortOrder) {
-                values.put(TasksContract.Columns.TASK_SORT_ORDER, sortOrder)
-            }
-            if(values.size() != 0) {
-                Log.d(TAG, "saveTask: Updating task")
-                activity?.contentResolver?.update(TasksContract.buildUriFromId(task.id),
-                    values, null, null)
-            }
-        } else {
-            Log.d(TAG, "saveTask: adding new task")
-            if (addedit_name.text.isNotEmpty()) {
-                values.put(TasksContract.Columns.TASK_NAME, addedit_name.text.toString())
-                if (addedit_description.text.isNotEmpty()) {
-                    values.put(TasksContract.Columns.TASK_DESCRIPTION,
-                        addedit_description.text.toString())
-                }
-                values.put(TasksContract.Columns.TASK_SORT_ORDER, sortOrder)
-                activity?.contentResolver?.insert(TasksContract.CONTENT_URI, values)
-            }
+        return newTask
+    }
+
+    private fun saveTask() {
+
+        val newTask = taskFromUi()
+        if (newTask != task) {
+            Log.d(TAG, "saveTask: saving task, id is ${newTask.id}")
+            task = viewModel.saveTask(newTask)
+            Log.d(TAG, "saveTask: id is ${task?.id}")
         }
     }
 
--- app/src/main/java/academy/learningrogramming/tasktimer/TaskTimerViewModel.kt
+++ app/src/main/java/academy/learningrogramming/tasktimer/TaskTimerViewModel.kt
@@ -1,6 +1,7 @@
 package academy.learningrogramming.tasktimer
 
 import android.app.Application
+import android.content.ContentValues
 import android.database.ContentObserver
 import android.database.Cursor
 import android.net.Uri
@@ -49,6 +50,33 @@ class TaskTimerViewModel (application: Application) : AndroidViewModel(applicati
                     sortOrder)
             databaseCursor.postValue(cursor)
         }
+    }
+
+    fun saveTask(task: Task): Task {
+        val values = ContentValues()
+
+        if (task.name.isNotEmpty()) {
+            values.put(TasksContract.Columns.TASK_NAME, task.name)
+            values.put(TasksContract.Columns.TASK_DESCRIPTION, task.description)
+            values.put(TasksContract.Columns.TASK_SORT_ORDER, task.sortOrder)
+
+            if (task.id == 0L) {
+                GlobalScope.launch {
+                    Log.d(TAG, "saveTask: adding new task")
+                    val uri = getApplication<Application>().contentResolver?.insert(TasksContract.CONTENT_URI, values)
+                    if (uri != null) {
+                        task.id = TasksContract.getId(uri)
+                        Log.d(TAG, "saveTask: new id is ${task.id}")
+                    }
+                }
+            } else {
+                GlobalScope.launch {
+                    Log.d(TAG, "saveTask: updating task")
+                    getApplication<Application>().contentResolver?.update(TasksContract.buildUriFromId(task.id), values, null, null)
+                }
+            }
+        }
+        return task
     }
 
     fun deleteTask(taskId: Long) {
